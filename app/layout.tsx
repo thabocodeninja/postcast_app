d2294fe30df5847c537b6e277719d502ce258fe9
Thabo <stylesbylethabo@gmail.com>
2025-03-24 15:31:01 +0200

    added clerk and convex configurations

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Manrope } from "next/font/google";
 import "./globals.css";
+import ConvexClerkProvider from "@/providers/ConvexClerkProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const manrope = Manrope({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Postcast",
@@ -26,12 +19,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+      <ConvexClerkProvider>
+          <html lang="en">
+          <body className={`${manrope.className}`}>
+              {children}
+          </body>
+          </html>
+        </ConvexClerkProvider>
   );
 }
