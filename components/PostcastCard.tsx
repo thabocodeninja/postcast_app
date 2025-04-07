b7450423e6be76cf9282b6319e556942b3ec7047
Thabo <stylesbylethabo@gmail.com>
2025-04-07 16:50:36 +0200

    generate thumbnail completed

--- components/PostcastCard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function PostcastCard() {
-  return (
-    <div>PostcastCard</div>
-  )
-}
-
-export default PostcastCard
--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,58 +1,71 @@
 'use client';
 
-import { sidebarLinks } from '@/constants'
-import { cn } from '@/lib/utils'
-import { SignedIn, SignedOut, useClerk } from '@clerk/nextjs';
-import Image from 'next/image'
-import Link from 'next/link'
-import { usePathname, useRouter } from 'next/navigation'
+import { SignedIn, UserButton, useUser } from '@clerk/nextjs'
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
-import { Button } from './ui/button';
-//import { useAudio } from '@/providers/AudioProvider';
+import Header from './Header';
+import Carousel from './Carousel';
+import { useQuery } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+import { useRouter } from 'next/navigation';
+import LoaderSpinner from './LoadSpinner';
+import { useAudio } from '@/providers/AudioProvider';
+import { cn } from '@/lib/utils';
 
-const LeftSidebar = () => {
-  const pathname = usePathname();
+const RightSidebar = () => {
+  const { user } = useUser();
+  const topPodcasters = useQuery(api.users.getTopUserByPodcastCount);
   const router = useRouter();
-  const { signOut } = useClerk();
+
   const { audio } = useAudio();
 
   return (
-    <section className={cn("left_sidebar h-[calc(100vh-5px)]", {
+    <section className={cn('right_sidebar h-[calc(100vh-5px)]', {
       'h-[calc(100vh-140px)]': audio?.audioUrl
     })}>
-      <nav className="flex flex-col gap-6">
-        <Link href="/" className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center">
-          <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
-          <h1 className="text-24 font-extrabold text-white max-lg:hidden">Podcast</h1>
-        </Link>
-
-        {sidebarLinks.map(({ route, label, imgURL }) => {
-          const isActive = pathname === route || pathname.startsWith(`${route}/`);
-
-          return <Link href={route} key={label} className={cn("flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start", {
-            'bg-nav-focus border-r-4 border-orange-1': isActive
-          })}>
-            <Image src={imgURL} alt={label} width={24} height={24} />
-            <p>{label}</p>
-          </Link>
-        })}
-      </nav>  
-      <SignedOut>
-        <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
-          <Button asChild className="text-16 w-full bg-orange-1 font-extrabold">
-            <Link href="/sign-in">Sign in</Link>
-          </Button>
-        </div>
-      </SignedOut>
       <SignedIn>
-        <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
-          <Button className="text-16 w-full bg-orange-1 font-extrabold" onClick={() => signOut(() => router.push('/'))}>
-            Log Out
-          </Button>
-        </div>
+        <Link href={`/profile/${user?.id}`} className="flex gap-3 pb-12">
+          <UserButton />
+          <div className="flex w-full items-center justify-between">
+            <h1 className="text-16 truncate font-semibold text-white-1">{user?.firstName} {user?.lastName}</h1>
+            <Image 
+              src="/icons/right-arrow.svg"
+              alt="arrow"
+              width={24}
+              height={24}
+            />
+          </div>
+        </Link>
       </SignedIn>
+      <section>
+        <Header headerTitle="Fans Like You" />
+        <Carousel fansLikeDetail={topPodcasters!}/>
+      </section>
+      <section className="flex flex-col gap-8 pt-12">
+        <Header headerTitle="Top Podcastrs" />
+        <div className="flex flex-col gap-6">
+          {topPodcasters?.slice(0, 3).map((podcaster) => (
+            <div key={podcaster._id} className="flex cursor-pointer justify-between" onClick={() => router.push(`/profile/${podcaster.clerkId}`)}>
+              <figure className="flex items-center gap-2">
+                <Image
+                  src={podcaster.imageUrl}
+                  alt={podcaster.name}
+                  width={44}
+                  height={44}
+                  className="aspect-square rounded-lg"
+                />
+                <h2 className="text-14 font-semibold text-white-1">{podcaster.name}</h2>
+              </figure>
+              <div className="flex items-center">
+                <p className="text-12 font-normal text-white-1">{podcaster.totalPodcasts} podcasts</p>
+              </div> 
+            </div>
+          ))}
+        </div>
+      </section>
     </section>
   )
 }
 
-export default LeftSidebar
+export default RightSidebar
