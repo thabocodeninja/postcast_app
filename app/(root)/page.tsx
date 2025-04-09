00356cbec06c6341fdf0f97d37b50c639fab8764
Thabo <stylesbylethabo@gmail.com>
2025-04-09 11:40:33 +0200

    carousel and openai completed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,29 @@
-"use client"
-
-import React from 'react'
-//import "./globals.css";
+"use client";
+import PodcastCard from '@/components/PodcastCard'
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import LoaderSpinner from '@/components/LoaderSpinner';
 
 const Home = () => {
-  return (
-    <div className='mt-9 flex flex-col gap-9 md:over-flow-hidden'>
-      <section className='flex flex-col gap-5'>
-        <div className='postcast_grid'>
-          <h1 className='text-20 font-bold text-white-1'>Trending Postcasts</h1>
+  const trendingPodcasts = useQuery(api.podcasts.getTrendingPodcasts);
 
+  if(!trendingPodcasts) return <LoaderSpinner />
+  
+  return (
+    <div className="mt-9 flex flex-col gap-9 md:overflow-hidden">
+      <section className='flex flex-col gap-5'>
+        <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
+
+        <div className="podcast_grid">
+          {trendingPodcasts?.map(({ _id, podcastTitle, podcastDescription, imageUrl }) => (
+            <PodcastCard 
+              key={_id}
+              imgUrl={imageUrl as string}
+              title={podcastTitle}
+              description={podcastDescription}
+              podcastId={_id}
+            />
+          ))}
         </div>
       </section>
     </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,16 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
-import ConvexClerkProvider from "@/providers/ConvexClerkProvider";
+import ConvexClerkProvider from "../providers/ConvexClerkProvider";
+import AudioProvider from "@/providers/AudioProvider";
 
 const manrope = Manrope({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Postcast",
-  description: "Generate Postcast next app using AI",
+  title: "Podcastr",
+  description: "Generate your podcasts using AI",
   icons: {
-    icon:'/icon/logo.svg'
+    icon: '/icons/logo.svg'
   }
 };
 
@@ -19,12 +20,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-      <ConvexClerkProvider>
-          <html lang="en">
+    <ConvexClerkProvider>
+      <html lang="en">
+        <AudioProvider>
           <body className={`${manrope.className}`}>
               {children}
           </body>
-          </html>
-        </ConvexClerkProvider>
+        </AudioProvider>
+      </html>
+    </ConvexClerkProvider>
   );
 }
